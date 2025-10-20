d46e4b5100fa48e35e1ae69ee459bcf8da34dc88
sttom <st.tomecekk@gmail.com>
2025-10-21 01:14:00 +0200

    genre_cantai?_of

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -12,6 +12,10 @@ def index(request):
 
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_books_genre = Genre.objects.count()  # Example of counting genres
+    # count books with 'the' in the title (case-insensitive)
+    num_books_containing_of = Book.objects.filter(title__icontains='of').count()
+    
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
@@ -21,6 +25,9 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_books_genre': num_books_genre,
+        'num_books_containing_of': num_books_containing_of,
+
     }
 
     # Render the HTML template index.html with the data in the context variable
